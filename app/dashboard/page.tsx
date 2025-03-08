59a1fcb958b724fdb61c8e4c957e0f6086baad97
fahrulrazix <fahrul.razix@gmail.com>
2025-03-08 14:28:10 +0700

    fixed prisma generate and add question mark

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -21,7 +21,7 @@ export default async function DashboardRoute() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
-  const data = await getData(user.id);
+  const data = await getData(user?.id);
   return (
     <div>
       <div className="flex items-center justify-between mb-4">
